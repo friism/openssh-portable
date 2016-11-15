c257530ecfa0a072ce9b9dbd47071c519f9c8815
Manoj Ampalam <manojampalam@live.com>
2016-11-15 10:11:48 -0800

    Cleaned up libssh

--- authfd.c
+++ authfd.c
@@ -96,11 +96,12 @@ ssh_get_authentication_socket(int *fdp)
 
 #ifdef WIN32_FIXME
 	{
-#define SSH_AGENT_ROOT "SOFTWARE\\SSH\\Agent"
+#define SSH_AGENT_REG_ROOT L"SOFTWARE\\SSH\\Agent"
+#define SSH_AGENT_PIPE_NAME L"\\\\.\\pipe\\ssh-agent"
 		HKEY agent_root = 0;
 		DWORD agent_pid = 0, tmp_size = 4, pipe_server_pid = 0xff;
 		HANDLE h;
-		RegOpenKeyEx(HKEY_LOCAL_MACHINE, SSH_AGENT_ROOT, 0, KEY_QUERY_VALUE, &agent_root);
+		RegOpenKeyExW(HKEY_LOCAL_MACHINE, SSH_AGENT_REG_ROOT, 0, KEY_QUERY_VALUE, &agent_root);
 		if (agent_root) {
 			RegQueryValueEx(agent_root, "ProcessId", 0, NULL, &agent_pid, &tmp_size);
 			RegCloseKey(agent_root);
--- authfile.c
+++ authfile.c
@@ -121,13 +121,11 @@ sshkey_load_file(int fd, struct sshbuf *blob)
 			goto out;
 		}
 	}
-#ifndef WIN32_FIXME//R
 	if ((st.st_mode & (S_IFSOCK|S_IFCHR|S_IFIFO)) == 0 &&
 	    st.st_size != (off_t)sshbuf_len(blob)) {
 		r = SSH_ERR_FILE_CHANGED;
 		goto out;
 	}
-#endif
 	r = 0;
 
  out:
